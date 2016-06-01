295933c13ac0639a7d33b98bc356615400d34d94
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2016-06-02 00:10:12 +0200

    bugfix concerning boolean '=' being recognized as an EprEqualityAtom

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/epr/EprTheory.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/epr/EprTheory.java
@@ -688,8 +688,12 @@ public class EprTheory implements ITheory {
 	public static boolean isQuantifiedEprAtom(Term idx) {
 		if (idx.getFreeVars().length > 0) {
 			if (idx instanceof ApplicationTerm) {
-				if (isEprPredicate(((ApplicationTerm) idx).getFunction())) return true;
-				if ((((ApplicationTerm) idx).getFunction()).getName().equals("=")) return true;
+				if (isEprPredicate(((ApplicationTerm) idx).getFunction())) 
+					return true;
+				if ((((ApplicationTerm) idx).getFunction()).getName().equals("=")
+						&& !(((ApplicationTerm) idx).getParameters()[0].getSort().getName().equals("Bool"))) 
+					return true;
+
 			}
 		}
 		return false;
@@ -700,6 +704,7 @@ public class EprTheory implements ITheory {
 		if (function.getName().equals("not")) return false;
 		if (function.getName().equals("or")) return false;
 		if (function.getName().equals("ite")) return false;
+		if (function.getName().equals("=")) return false;
 		return true;
 	}
 	
