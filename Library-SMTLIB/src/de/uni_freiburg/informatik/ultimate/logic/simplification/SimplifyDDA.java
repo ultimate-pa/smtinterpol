23d48c470daf516a582ba5af787fdf2c37f7efb7
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-09-02 18:11:04 +0200

    renaming \bm_([\w|\d]+) to m\1 in Library-SMTLIB

--- Library-SMTLIB/src/de/uni_freiburg/informatik/ultimate/logic/simplification/SimplifyDDA.java
+++ Library-SMTLIB/src/de/uni_freiburg/informatik/ultimate/logic/simplification/SimplifyDDA.java
@@ -240,20 +240,20 @@ public class SimplifyDDA extends NonRecursive {
 							mNegated, params[mParamCtr], mContext));
 				} else if (mParamCtr < params.length) {
 					// The context contains:
-					//  param[len-1] ... param[m_paramCtr] 
-					//      simplify(param[m_paramCtr-2])... simplify(param[0])
-					// we need to replace param[m_paramCtr]
-					// by simplify(param[m_paramCtr-1]).
+					//  param[len-1] ... param[mParamCtr] 
+					//      simplify(param[mParamCtr-2])... simplify(param[0])
+					// we need to replace param[mParamCtr]
+					// by simplify(param[mParamCtr-1]).
 					/*  this is dangerous:  the simplified formulas may depend
 					 * on their context, therefore we cannot simply merge them.
-					for (int i = 0; i < m_paramCtr; i++) {
-						m_context.pop();
+					for (int i = 0; i < mParamCtr; i++) {
+						mContext.pop();
 					}
-					for (int i = m_paramCtr-1; i >= 0; i--) {
+					for (int i = mParamCtr-1; i >= 0; i--) {
 						Term sibling = simplifier.negateSibling(
 							params[i], connective, i, params.length);
 						sibling = simplifier.createSimplify(sibling);
-						m_context.push(sibling);
+						mContext.push(sibling);
 					}
 					*/
 					mContext.pop();
@@ -262,8 +262,8 @@ public class SimplifyDDA extends NonRecursive {
 							mNegated, params[mParamCtr], mContext));
 				} else {
 					/*
-					for (int i = 0; i < m_paramCtr-1; i++) {
-						m_context.pop();
+					for (int i = 0; i < mParamCtr-1; i++) {
+						mContext.pop();
 					}
 					*/
 				}
@@ -714,7 +714,7 @@ public class SimplifyDDA extends NonRecursive {
 	 * @param mTerm whose Sort is Boolean
 	 */
 	public Term getSimplifiedTerm(Term inputTerm) throws SMTLIBException {
-//		m_Logger.debug("Simplifying " + term);
+//		mLogger.debug("Simplifying " + term);
 		/* We can only simplify boolean terms. */
 		if (!inputTerm.getSort().getName().equals("Bool")) {
 			return inputTerm;
