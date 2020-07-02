8df4f9166b5240aebe4b01354da944e47c550208
Leonard Fichtner <leonard.fichtner@web.de>
2020-07-02 23:25:34 +0200

    Some refactoring and started with easy extension method

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/muses/UnexploredMap.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/muses/UnexploredMap.java
@@ -2,9 +2,9 @@ package de.uni_freiburg.informatik.ultimate.smtinterpol.muses;
 
 import java.util.BitSet;
 
-public class Map {
+public class UnexploredMap {
 
-	public Map() {
+	public UnexploredMap() {
 
 	}
 
