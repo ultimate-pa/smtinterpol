336608f70bddc664c83a6754944667e56de4cb96
Leonard Fichtner <leonard.fichtner@web.de>
2020-07-10 18:22:21 +0200

    Refactoring + Implemented getImpliedCrits in UnexploredMap

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/muses/UnexploredMap.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/muses/UnexploredMap.java
@@ -2,6 +2,7 @@ package de.uni_freiburg.informatik.ultimate.smtinterpol.muses;
 
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.function.Function;
 
 import de.uni_freiburg.informatik.ultimate.smtinterpol.dpll.Clause;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.dpll.DPLLEngine;
@@ -17,16 +18,22 @@ public class UnexploredMap {
 
 	DPLLEngine mEngine;
 	Translator mTranslator;
+	boolean mMapModifiedSinceLastSolve;
+	BitSet mImpliedCrits;
+	BitSet mMaximalUnexploredSubset;
+	BitSet mLastWorkingSet;
 
 	public UnexploredMap(final DPLLEngine engine, final Translator translator) {
 		mEngine = engine;
 		mTranslator = translator;
+		mMapModifiedSinceLastSolve = true;
 	}
 
 	/**
 	 * Blocks all supersets of the given unsatisfiable set (since they are also unsatisfiable).
 	 */
 	public void BlockUp(final BitSet unsatSet) {
+		mMapModifiedSinceLastSolve = true;
 		final ArrayList<Literal> clause = new ArrayList<>();
 		for (int i = unsatSet.nextSetBit(0); i >= 0; i = unsatSet.nextSetBit(i + 1)) {
 			clause.add(mTranslator.translate2Atom(i).negate());
@@ -38,6 +45,7 @@ public class UnexploredMap {
 	 * Blocks all subsets of the given satisfiable set (since they are also satisfiable).
 	 */
 	public void BlockDown(final BitSet satSet) {
+		mMapModifiedSinceLastSolve = true;
 		final ArrayList<Literal> clause = new ArrayList<>();
 		final BitSet notInSatSet = (BitSet) satSet.clone();
 		notInSatSet.flip(0, mTranslator.getNumberOfConstraints());
@@ -48,10 +56,12 @@ public class UnexploredMap {
 	}
 
 	/**
-	 * Gets a maximal unexplored subset (maximal in the sense that every superset of this set is explored) of the given
-	 * working set. Returns a BitSet with all bits set to false, if there is no maximal unexplored subset.
+	 * Finds a maximal unexplored subset and the implied crits for the given working set. They can then be accessed by
+	 * {@link getMaximalUnexploredSubset()} and {@link getImpliedCrits}.
 	 */
-	public BitSet getMaximalUnexploredSubsetOf(final BitSet workingSet) {
+	private boolean findMaximalUnexploredSubsetAndImpliedCrits(final BitSet workingSet) {
+		mMapModifiedSinceLastSolve = false;
+		mLastWorkingSet = (BitSet) workingSet.clone();
 		final BitSet notInWorkingSet = (BitSet) workingSet.clone();
 		notInWorkingSet.flip(0, mTranslator.getNumberOfConstraints());
 		mEngine.push();
@@ -61,26 +71,59 @@ public class UnexploredMap {
 			mEngine.addClause(new Clause(unitClause, mEngine.getAssertionStackLevel()));
 		}
 		if (mEngine.solve()) {
-			final BitSet maximalUnexplored = collectTrueAtomsOf(workingSet);
+			mMaximalUnexploredSubset = collectAtomsWithCriteria(workingSet, this::isSetToTrue);
+			mImpliedCrits = collectAtomsWithCriteria(workingSet, this::isImpliedToTrue);
 			mEngine.pop(1);
-			return maximalUnexplored;
-		}else {
+			return true;
+		} else {
+			mMaximalUnexploredSubset = new BitSet();
+			mImpliedCrits = new BitSet();
 			mEngine.pop(1);
-			return new BitSet();
+			return false;
 		}
 	}
 
 	/**
-	 * Collects all Atoms that are contained in workingSet and are set to True. Assumes, that all
-	 * Atoms have been decided (else NullPointerExceptions may occur).
+	 * If it has not already been found, this method finds a maximal unexplored subset of the given workingSet.
+	 * If no maximal unexplored subset has been found, it returns a BitSet with all values set to false.
 	 */
-	private BitSet collectTrueAtomsOf(final BitSet workingSet) {
+	public BitSet getMaximalUnexploredSubsetOf(final BitSet workingSet) {
+		if (mMapModifiedSinceLastSolve || workingSet.equals(mLastWorkingSet)) {
+			findMaximalUnexploredSubsetAndImpliedCrits(workingSet);
+		}
+		return mMaximalUnexploredSubset;
+	}
+
+	/**
+	 * If this no ImpliedCrits have been found, it returns a BitSet with all values set to false.
+	 */
+	public BitSet getImpliedCritsOf(final BitSet workingSet) {
+		if (mMapModifiedSinceLastSolve || workingSet.equals(mLastWorkingSet)) {
+			findMaximalUnexploredSubsetAndImpliedCrits(workingSet);
+		}
+		return mImpliedCrits;
+	}
+
+	/**
+	 * Collects all Atoms that are contained in workingSet and fulfill the given criteria. The criteria Function takes
+	 * the Number of the atom and returns True if it fulfills certain criteria, false otherwise. Assumes, that all Atoms
+	 * have been decided (else NullPointerExceptions may occur).
+	 */
+	private BitSet collectAtomsWithCriteria(final BitSet workingSet, final Function<Integer, Boolean> criteria) {
 		final BitSet model = new BitSet(mTranslator.getNumberOfConstraints());
 		for (int i = workingSet.nextSetBit(0); i >= 0; i = workingSet.nextSetBit(i + 1)) {
-			if (mTranslator.translate2Atom(i).getDecideStatus().getSign() == 1) {
+			if (criteria.apply(i)) {
 				model.set(i);
 			}
 		}
 		return model;
+	}
+
+	private Boolean isSetToTrue(final int atomNumber) {
+		return mTranslator.translate2Atom(atomNumber).getDecideStatus().getSign() == 1;
+	}
+
+	private Boolean isImpliedToTrue(final int atomNumber) {
+		return isSetToTrue(atomNumber) && mTranslator.translate2Atom(atomNumber).getDecideLevel() == 0;
 	}
 }
