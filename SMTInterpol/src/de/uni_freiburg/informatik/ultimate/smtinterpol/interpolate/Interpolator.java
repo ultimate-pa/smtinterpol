f5c2ec17b7e97808e5aa5117470905fb0a5912fe
henkele <henkele@informatik.uni-freiburg.de>
2020-10-01 16:06:42 +0200

    use mSymbolOccurrenceInfo instead of mSymbolPartition when computing occurrence

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/Interpolator.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/Interpolator.java
@@ -821,7 +821,7 @@ public class Interpolator extends NonRecursive {
 		if (term instanceof ConstantTerm) {
 			return mFullOccurrence;
 		}
-		Occurrence result = mSymbolPartition.get(term);
+		Occurrence result = mSymbolOccurrenceInfos.get(term);
 		if (result == null) {
 			if (term instanceof ApplicationTerm && ((ApplicationTerm) term).getFunction().isIntern()
 					&& !((ApplicationTerm) term).getFunction().getName().startsWith("@AUX")) {
