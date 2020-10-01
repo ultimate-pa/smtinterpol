81dd5cc5e3183073bbe9112aadb8f64737631aae
henkele <henkele@informatik.uni-freiburg.de>
2020-10-01 16:08:26 +0200

    color @AUX symbols

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/Interpolator.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/Interpolator.java
@@ -1590,7 +1590,7 @@ public class Interpolator extends NonRecursive {
 				todoStack.add(params[i]);
 			}
 			// Add symbol if it is not an internal symbol
-			if (!funSymbol.isIntern()) {
+			if (!funSymbol.isIntern() || funSymbol.getName().startsWith("@AUX")) {
 				result.add(symbol);
 			}
 		}
