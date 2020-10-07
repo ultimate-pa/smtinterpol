0a98bafa0e6399c6eeeb788ecc18c7c520fcfcac
Andrea Qosja <qosja.andrea@yahoo.com>
2020-10-07 17:36:24 +0200

    Added webworker functionality, some bugs to fix.

---
+++ Webinterface/src/main/java/de/uni_freiburg/informatik/ultimate/smtinterpol/web/SolverInterface.java
@@ -0,0 +1,10 @@
+package de.uni_freiburg.informatik.ultimate.smtinterpol.web;
+
+import org.teavm.jso.JSObject;
+
+public interface SolverInterface extends JSObject {
+
+    String runSMTInterpol(String input);
+
+    void requestTermination();
+}
