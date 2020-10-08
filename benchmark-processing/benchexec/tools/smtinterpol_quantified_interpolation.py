ebe7f3f3050cf1b917eab00889be4d34fbca6138
henkele <henkele@informatik.uni-freiburg.de>
2020-10-08 18:22:23 +0200

    benchmark files

---
+++ benchmark-processing/benchexec/tools/smtinterpol_quantified_interpolation.py
@@ -0,0 +1,37 @@
+# This file is part of BenchExec, a framework for reliable benchmarking:
+# https://github.com/sosy-lab/benchexec
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import subprocess
+
+import benchexec.util as util
+import benchexec.tools.smtlib2_quantified_interpolation
+
+class Tool(benchexec.tools.smtlib2_quantified_interpolation.Smtlib2Tool):
+    """
+    Tool info for SMTInterpol.
+    """
+
+    def executable(self):
+        return util.find_executable("java")
+
+    def version(self, executable):
+        stderr = subprocess.Popen(
+            self.cmdline(executable, ["-version"], []), stderr=subprocess.PIPE
+        ).communicate()[1]
+        stderr = util.decode_to_string(stderr)
+        line = next(
+            line for line in stderr.splitlines() if line.startswith("SMTInterpol")
+        )
+        line = line.replace("SMTInterpol", "")
+        return line.strip()
+
+    def name(self):
+        return "SMTInterpol"
+
+    def cmdline(self, executable, options, tasks, propertyfile=None, rlimits={}):
+        assert len(tasks) <= 1, "only one inputfile supported"
+        return [executable, "-jar", "smtinterpol.jar"] + options + tasks
