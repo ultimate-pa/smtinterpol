d2a37682144148e2c8a20057e60fea6ae039a6fb
Andrea Qosja <qosja.andrea@yahoo.com>
2020-11-02 11:02:24 +0100

    Changed Doc not final, and removed some not needed lines.

--- Webinterface/src/main/webapp/webworker.js
+++ Webinterface/src/main/webapp/webworker.js
@@ -1,6 +1,5 @@
 importScripts("classes.js");
 var runWebInterface;
-var counter = 1;
 
 self.addEventListener('message', (ev)=>{
     console.log('Web worker started.');
@@ -8,21 +7,10 @@ self.addEventListener('message', (ev)=>{
     let data = ev.data;
     switch(data){
         default:
-            console.log("Versuch aufzurufen, counter: ", counter);
-            // Zuweiss auf eine Variable kann man vielleicht nicht stoppen.
             var output = runWebInterface.runSMTInterpol(data);
-            console.log("Aufruf erfolgreich, counter: ", counter);
             self.postMessage(output);
-            counter += 1;
             self.close();
             break;
-            /*
-        case 'Stop SMTInterpol':
-            console.log("Versuch thread zu stoppen");
-            runWebInterface.requestTermination();
-            console.log('Nach Webworker thread is stopped');
-            break;
-            */
     }
 });
 
