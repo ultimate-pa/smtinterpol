18ab3d6c04decbc842af92f19fd962033ff71f2f
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2021-04-30 16:22:51 +0200

    fix set files

--- SMTInterpolTest/benchexec/run-experiments.sh
+++ SMTInterpolTest/benchexec/run-experiments.sh
@@ -5,7 +5,6 @@ source "/root/.sdkman/bin/sdkman-init.sh"
 TOOL_DIR="/storage/repos/smtinterpol"
 BENCH_BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 RESULT_DIR="${BENCH_BASE}/results/"
-OPTS=()
 
 echo "Running Benchexec for SMTInterpol"
 sdk use java 8.0.275.open-adpt
@@ -14,7 +13,7 @@ version=$(ant get-version | grep -oP "Version is \K.*")
 ant
 jar_file="smtinterpol-${version}.jar"
 cp dist/${jar_file} smtinterpol.jar
-if [ ! $? -eq 0 ] ; then echo "could not copy smtinterpol.jar" ; exit 1 ; fi 
+if [ ! $? -eq 0 ] ; then echo "could not copy smtinterpol.jar" ; exit 1 ; fi
 /storage/repos/benchexec/bin/benchexec --no-container --no-compress-results --maxLogfileSize=-1 --numOfThreads=14 -o "${RESULT_DIR}" "${BENCH_BASE}/defs/bv-preprocessing-tests.xml"
 popd > /dev/null
 
