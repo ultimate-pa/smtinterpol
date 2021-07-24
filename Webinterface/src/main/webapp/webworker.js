27e1190c8e6a635a2d699247ff80ef84793c1068
Jochen Hoenicke <hoenicke@gmail.com>
2021-07-24 19:11:43 +0200

    some cleanups

--- Webinterface/src/main/webapp/webworker.js
+++ Webinterface/src/main/webapp/webworker.js
@@ -1,17 +1,10 @@
 importScripts("classes.js");
 var runWebInterface;
 
-self.addEventListener('message', (ev)=>{
-    console.log('Web worker started.');
-
-    let data = ev.data;
-    switch(data){
-        default:
-        runWebInterface.runSMTInterpol(data);
-	self.postMessage("@EXIT");
-        self.close();
-        break;
-    }
+onmessage = ((ev) => {
+    runWebInterface.runSMTInterpol(ev.data);
+    self.postMessage("@EXIT");
+    self.close();
 });
 
 main();
