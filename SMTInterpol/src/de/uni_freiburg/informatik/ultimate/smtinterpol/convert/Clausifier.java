b65ea2ec4798eef6d6a2deb222262764177eab9d
Jochen Hoenicke <hoenicke@gmail.com>
2022-02-08 13:11:49 +0100

    Bugfix: use correct selector in generic case.

--- SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/convert/Clausifier.java
+++ SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/convert/Clausifier.java
@@ -1771,7 +1771,7 @@ public class Clausifier {
 				// substitute argument TermVariables with the according selector function
 				int s_i = 0;
 				for (final String sel : c.getSelectors()) {
-					final Term selTerm = theory.term(theory.getFunctionSymbol(sel), dataTerm);
+					final Term selTerm = theory.term(sel, dataTerm);
 					argSubs.put(term.getVariables()[c_i][s_i++], selTerm);
 				}
 				rule = ProofConstants.AUX_MATCH_CASE;
