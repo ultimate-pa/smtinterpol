393a91c0f6ba096509db5086d6cdd61da698972d
Damien Zufferey <124589358+damien-zufferey-sonarsource@users.noreply.github.com>
2023-06-05 10:09:30 +0200

    add option for reproducible-resource-limit (#140)

--- SMTInterpolTest/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/EqInterpolatorRealTest.java
+++ SMTInterpolTest/src/de/uni_freiburg/informatik/ultimate/smtinterpol/interpolate/EqInterpolatorRealTest.java
@@ -106,7 +106,7 @@ public class EqInterpolatorRealTest {
 		@SuppressWarnings("unchecked")
 		final Set<String>[] partition = new Set[] { empty, empty };
 		mInterpolator = new Interpolator(mSolver.getLogger(), null, null, mTheory, partition, new int[partition.length],
-				mSolver.getTimeoutHandler());
+				mSolver.getTerminationRequest());
 		final HashSet<Term> bsubTerms = mInterpolator.getSubTerms(bSmt);
 		final HashSet<Term> asubTerms = mInterpolator.getSubTerms(aSmt);
 		for (final Term sub : asubTerms) {
