ultimate-pa/smtinterpol

commit d565c5509fd5e2a6f99ad1b53a5a32328ebbb999

Some bug fixes

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/quant/InstantiationManager.java
@@ -41,13 +41,10 @@ import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.Config;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.Clausifier;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.SMTAffineTerm;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.dpll.Literal;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.cclosure.CCTerm;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.epr.util.Pair;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.linar.InfinitesimalNumber;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.linar.LASharedTerm;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.linar.LinVar;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.linar.MutableAffineTerm;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.QuantifierTheory.InstanceOrigin;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.QuantifierTheory.InstantiationMethod;
@@ -55,6 +52,7 @@ import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.Substitution
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.dawg.Dawg;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.ematching.EMatching;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant.ematching.EMatching.SubstitutionInfo;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.util.Polynomial;
 
 /**
  * This class takes care of clause, literal and term instantiation.
@@ -1089,31 +1087,32 @@ public class InstantiationManager {
 	 * @return a MutableAffineTerm if each summand of the SMTAffineTerm either is ground or has an equivalent Term
 	 *         storing a LinVar, null otherwise.
 	 */
-	private MutableAffineTerm buildMutableAffineTerm(final SMTAffineTerm smtAff,
+	private MutableAffineTerm buildMutableAffineTerm(final Polynomial smtAff,
 			final Map<Term, Term> sharedForQuantSmds) {
 		final MutableAffineTerm at = new MutableAffineTerm();
-		for (final Entry<Term, Rational> entry : smtAff.getSummands().entrySet()) {
-			final Term sharedTerm;
-			if (entry.getKey().getFreeVars().length == 0) {
-				sharedTerm = entry.getKey();
-			} else {
-				sharedTerm = sharedForQuantSmds.get(entry.getKey());
-				if (sharedTerm == null) {
-					return null;
+		for (final Entry<Map<Term, Integer>, Rational> entry : smtAff.getSummands().entrySet()) {
+			final Polynomial sharedMonomial = new Polynomial();
+			sharedMonomial.add(entry.getValue());
+			for (final Entry<Term, Integer> factor : entry.getKey().entrySet()) {
+				final Term sharedTerm;
+				if (factor.getKey().getFreeVars().length == 0) {
+					sharedTerm = factor.getKey();
+				} else {
+					sharedTerm = sharedForQuantSmds.get(factor.getKey());
+					if (sharedTerm == null) {
+						return null;
+					}
+				}
+				for (int i = 0; i < factor.getValue(); i++) {
+					sharedMonomial.mul(new Polynomial(sharedTerm));
 				}
 			}
-			final LASharedTerm laTerm = mClausifier.getLATerm(sharedTerm);
-			final Rational coeff = entry.getValue();
-			if (laTerm != null) {
-				for (final Map.Entry<LinVar, Rational> summand : laTerm.getSummands().entrySet()) {
-					at.add(coeff.mul(summand.getValue()), summand.getKey());
-				}
-				at.add(coeff.mul(laTerm.getOffset()));
-			} else {
+			final MutableAffineTerm matMonomial = mClausifier.toMutableAffineTerm(sharedMonomial);
+			if (matMonomial == null) {
 				return null;
 			}
+			at.add(entry.getValue(), matMonomial);
 		}
-		at.add(smtAff.getConstant());
 		return at;
 	}
 
@@ -1421,8 +1420,8 @@ public class InstantiationManager {
 	 * @return the InstanceValue of the substituted literal.
 	 */
 	private InstanceValue evaluateLAEqualityKnownShared(final QuantEquality qEq, final Map<Term, Term> sharedForQuant) {
-		final SMTAffineTerm diff = new SMTAffineTerm(qEq.getLhs());
-		diff.add(Rational.MONE, new SMTAffineTerm(qEq.getRhs()));
+		final Polynomial diff = new Polynomial(qEq.getLhs());
+		diff.add(Rational.MONE, qEq.getRhs());
 
 		final MutableAffineTerm at = buildMutableAffineTerm(diff, sharedForQuant);
 		if (at != null) {
@@ -1449,24 +1448,28 @@ public class InstantiationManager {
 	 * @return the InstanceValue of the substituted literal.
 	 */
 	private InstanceValue evaluateLAEquality(final QuantEquality qEq, final List<Term> subs) {
-		final SMTAffineTerm diff = new SMTAffineTerm(qEq.getLhs());
+		final Polynomial diff = new Polynomial(qEq.getLhs());
 		diff.add(Rational.MONE, qEq.getRhs());
 
 		final QuantClause qClause = qEq.getClause();
 		final TermFinder finder = new TermFinder(qClause.getVars(), subs);
-		final SMTAffineTerm smtAff = finder.findEquivalentAffine(diff);
-		if (smtAff != null) {
-			final InfinitesimalNumber upperBound = mQuantTheory.mLinArSolve.getUpperBound(mClausifier, smtAff);
-			smtAff.negate();
-			final InfinitesimalNumber negLowerBound = mQuantTheory.mLinArSolve.getUpperBound(mClausifier, smtAff);
-			if (upperBound.signum() == 0 && negLowerBound.signum() == 0) {
-				return InstanceValue.TRUE;
-			} else if (upperBound.signum() < 0 || negLowerBound.signum() < 0) {
-				return InstanceValue.FALSE;
-			}
-			return InstanceValue.ONE_UNDEF;
+		final Polynomial smtAff = finder.findEquivalentAffine(diff);
+		if (smtAff == null) {
+			return mDefaultValueForLitDawgs;
 		}
-		return mDefaultValueForLitDawgs;
+		final MutableAffineTerm mat = mClausifier.toMutableAffineTerm(smtAff);
+		if (mat == null) {
+			return mDefaultValueForLitDawgs;
+		}
+		final InfinitesimalNumber upperBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
+		smtAff.mul(Rational.MONE);
+		final InfinitesimalNumber negLowerBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
+		if (upperBound.signum() == 0 && negLowerBound.signum() == 0) {
+			return InstanceValue.TRUE;
+		} else if (upperBound.signum() < 0 || negLowerBound.signum() < 0) {
+			return InstanceValue.FALSE;
+		}
+		return InstanceValue.ONE_UNDEF;
 	}
 
 	/**
@@ -1510,16 +1513,20 @@ public class InstantiationManager {
 	 */
 	private InstanceValue evaluateBoundConstraint(final QuantBoundConstraint qBc, final List<Term> subs) {
 		final TermFinder finder = new TermFinder(qBc.getClause().getVars(), subs);
-		final SMTAffineTerm affine = finder.findEquivalentAffine(qBc.getAffineTerm());
+		final Polynomial affine = finder.findEquivalentAffine(qBc.getAffineTerm());
 		if (affine == null) {
 			return mDefaultValueForLitDawgs;
 		}
-		final InfinitesimalNumber upperBound = mQuantTheory.mLinArSolve.getUpperBound(mClausifier, affine);
+		final MutableAffineTerm mat = mClausifier.toMutableAffineTerm(affine);
+		if (mat == null) {
+			return mDefaultValueForLitDawgs;
+		}
+		final InfinitesimalNumber upperBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
 		if (upperBound.lesseq(InfinitesimalNumber.ZERO)) {
 			return InstanceValue.TRUE;
 		} else {
-			affine.negate();
-			final InfinitesimalNumber lowerBound = mQuantTheory.mLinArSolve.getUpperBound(mClausifier, affine);
+			mat.mul(Rational.MONE);
+			final InfinitesimalNumber lowerBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
 			if (lowerBound.less(InfinitesimalNumber.ZERO)) {
 				return InstanceValue.FALSE;
 			} else {
@@ -1558,22 +1565,31 @@ public class InstantiationManager {
 			return mTerms.get(term);
 		}
 
-		SMTAffineTerm findEquivalentAffine(final SMTAffineTerm smtAff) {
-			for (final Term smd : smtAff.getSummands().keySet()) {
-				enqueueWalker(new FindTerm(smd));
+		Polynomial findEquivalentAffine(final Polynomial smtAff) {
+			for (final Map<Term, Integer> monom : smtAff.getSummands().keySet()) {
+				for (final Term smd : monom.keySet()) {
+					enqueueWalker(new FindTerm(smd));
+				}
 			}
 			run();
 			return buildEquivalentAffine(smtAff);
 		}
 
-		private SMTAffineTerm buildEquivalentAffine(final SMTAffineTerm smtAff) {
-			final SMTAffineTerm instAff = new SMTAffineTerm();
-			for (final Entry<Term, Rational> smd : smtAff.getSummands().entrySet()) {
-				final Term inst = mTerms.get(smd.getKey());
-				if (inst == null) {
-					return null;
+		private Polynomial buildEquivalentAffine(final Polynomial smtAff) {
+			final Polynomial instAff = new Polynomial();
+			for (final Entry<Map<Term, Integer>, Rational> smd : smtAff.getSummands().entrySet()) {
+				final Polynomial instMonom = new Polynomial();
+				for (final Entry<Term, Integer> factors : smd.getKey().entrySet()) {
+					final Term inst = mTerms.get(factors.getKey());
+					if (inst == null) {
+						return null;
+					}
+					final Polynomial instPoly = new Polynomial(inst);
+					for (int i = 0; i < factors.getValue(); i++) {
+						instMonom.mul(instPoly);
+					}
 				}
-				instAff.add(smd.getValue(), inst);
+				instAff.add(smd.getValue(), instMonom);
 			}
 			instAff.add(smtAff.getConstant());
 			return instAff;
@@ -1604,10 +1620,12 @@ public class InstantiationManager {
 								enqueueWalker(new FindTerm(arg));
 							}
 						} else if (func.getName() == "+" || func.getName() == "*" || func.getName() == "-") {
-							final SMTAffineTerm smtAff = new SMTAffineTerm(mTerm);
+							final Polynomial smtAff = new Polynomial(mTerm);
 							enqueueWalker(new FindSharedAffine(mTerm, smtAff));
-							for (final Term smd : smtAff.getSummands().keySet()) {
-								enqueueWalker(new FindTerm(smd));
+							for (final Map<Term,Integer> monom : smtAff.getSummands().keySet()) {
+								for (final Term smd : monom.keySet()) {
+									enqueueWalker(new FindTerm(smd));
+								}
 							}
 						}
 					}
@@ -1647,16 +1665,16 @@ public class InstantiationManager {
 
 		class FindSharedAffine implements Walker {
 			private final Term mTerm;
-			private final SMTAffineTerm mSmtAff;
+			private final Polynomial mSmtAff;
 
-			FindSharedAffine(final Term term, final SMTAffineTerm smtAff) {
+			FindSharedAffine(final Term term, final Polynomial smtAff) {
 				mTerm = term;
 				mSmtAff = smtAff;
 			}
 
 			@Override
 			public void walk(final NonRecursive engine) {
-				final SMTAffineTerm instAffine = buildEquivalentAffine(mSmtAff);
+				final Polynomial instAffine = buildEquivalentAffine(mSmtAff);
 				if (instAffine != null) {
 					final Term instTerm = instAffine.toTerm(mTerm.getSort());
 					// Note: This will often not find a CC term.

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/convert/Clausifier.java
@@ -1100,14 +1100,19 @@ public class Clausifier {
 				boolean needsLA = term instanceof ConstantTerm;
 				if (term instanceof ApplicationTerm) {
 					final String func = ((ApplicationTerm) term).getFunction().getName();
-					if (func.equals("+") || func.equals("-") || func.equals("*") || func.equals("to_real")) {
+					if (func.equals("+") || func.equals("-") || func.equals("to_real")) {
 						needsLA = true;
 					}
+					if (func.equals("*")) {
+						needsLA = true; // !isMonomial(term) || !mLATerms.containsKey(term);
+					}
 				}
-				if (needsLA && !isMonomial(term)) {
+				if (needsLA) {
 					final MutableAffineTerm mat = createMutableAffinTerm(new Polynomial(term), source);
 					assert mat.getConstant().mEps == 0;
-					shareLATerm(term, new LASharedTerm(term, mat.getSummands(), mat.getConstant().mReal));
+					if (!mLATerms.containsKey(term)) {
+						shareLATerm(term, new LASharedTerm(term, mat.getSummands(), mat.getConstant().mReal));
+					}
 				}
 			}
 			if (term.getSort() == term.getTheory().getBooleanSort()
@@ -1197,6 +1202,26 @@ public class Clausifier {
 				final Term monomial = createMonomial(summand.getKey());
 				final LinVar lv = createLinVar(monomial, source);
 				res.add(coeff, lv);
+			}
+		}
+		return res;
+	}
+
+	public MutableAffineTerm toMutableAffineTerm(final Polynomial poly) {
+		final MutableAffineTerm res = new MutableAffineTerm();
+		for (final Map.Entry<Map<Term, Integer>, Rational> summand : poly.getSummands().entrySet()) {
+			final Rational coeff = summand.getValue();
+			if (summand.getKey().isEmpty()) {
+				res.add(coeff);
+			} else {
+				final Term monomial = createMonomial(summand.getKey());
+				final LASharedTerm laShared = getLATerm(monomial);
+				if (laShared == null) {
+					return null;
+				}
+				assert laShared.getSummands().size() == 1 && laShared.getOffset() == Rational.ZERO
+						&& laShared.getSummands().values().iterator().next() == Rational.ONE;
+				res.add(coeff, laShared.getSummands().keySet().iterator().next());
 			}
 		}
 		return res;
@@ -2041,7 +2066,7 @@ public class Clausifier {
 	 */
 	public static Term checkAndGetTrivialEquality(final Term lhs, final Term rhs, final Theory theory) {
 		// This code corresponds to the check in createEqualityProxy(...)
-		final SMTAffineTerm diff = new SMTAffineTerm(lhs);
+		final Polynomial diff = new Polynomial(lhs);
 		diff.add(Rational.MONE, rhs);
 		if (diff.isConstant()) {
 			if (diff.getConstant().equals(Rational.ZERO)) {
@@ -2050,7 +2075,7 @@ public class Clausifier {
 				return theory.mFalse;
 			}
 		} else {
-			diff.div(diff.getGcd());
+			diff.mul(diff.getGcd().inverse());
 			Sort sort = lhs.getSort();
 			// Normalize equality to integer logic if all variables are integer.
 			if (theory.getLogic().isIRA() && sort.getName().equals("Real") && diff.isAllIntSummands()) {

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/convert/SMTAffineTerm.java
@@ -65,9 +65,17 @@ public final class SMTAffineTerm {
 			Rational factor = Rational.ONE;
 			if (subterm instanceof ApplicationTerm && ((ApplicationTerm) subterm).getFunction().getName() == "*") {
 				final Term[] params = ((ApplicationTerm) subterm).getParameters();
-				assert params.length == 2;
-				factor = convertConstant((ConstantTerm) parseConstant(params[0]));
-				subterm = params[1];
+				final Term constant = parseConstant(params[0]);
+				if (constant instanceof ConstantTerm) {
+					factor = convertConstant((ConstantTerm) constant);
+					if (params.length == 2) {
+						subterm = params[1];
+					} else {
+						final Term[] remainder = new Term[params.length - 1];
+						System.arraycopy(params, 1, remainder, 0, remainder.length);
+						subterm = subterm.getTheory().term(((ApplicationTerm) subterm).getFunction(), remainder);
+					}
+				}
 			}
 			if (subterm instanceof ApplicationTerm && ((ApplicationTerm) subterm).getFunction().getName() == "-"
 					&& ((ApplicationTerm) subterm).getParameters().length == 1) {

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/proof/ProofSimplifier.java
@@ -2047,7 +2047,7 @@ public class ProofSimplifier extends TermTransformer {
 				final Term[] expectedArgs = new Term[lhsArgs.length];
 				expectedArgs[0] = lhsArgs[0];
 				for (int i = 1; i < lhsArgs.length; i++) {
-					final SMTAffineTerm affineTerm = new SMTAffineTerm();
+					final Polynomial affineTerm = new Polynomial();
 					affineTerm.add(Rational.MONE, lhsArgs[i]);
 					expectedArgs[i] = affineTerm.toTerm(lhsArgs[i].getSort());
 				}

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/quant/QuantBoundConstraint.java
@@ -19,7 +19,7 @@
 package de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant;
 
 import de.uni_freiburg.informatik.ultimate.logic.Term;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.SMTAffineTerm;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.util.Polynomial;
 
 /**
  * A quantified bound constraint atom of the form "SMTAffineTerm <= 0".
@@ -29,15 +29,15 @@ import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.SMTAffineTerm;
  */
 public class QuantBoundConstraint extends QuantLiteral {
 
-	private final SMTAffineTerm mSmtAff;
+	private final Polynomial mSmtAff;
 
-	public QuantBoundConstraint(Term term, SMTAffineTerm smtAff) {
+	public QuantBoundConstraint(Term term, Polynomial smtAff) {
 		super(term);
 		mSmtAff = smtAff;
 		mNegated = new NegQuantLiteral(this);
 	}
 
-	public SMTAffineTerm getAffineTerm() {
+	public Polynomial getAffineTerm() {
 		return mSmtAff;
 	}
 }

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/quant/QuantClause.java
@@ -36,12 +36,12 @@ import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.logic.Theory;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.SMTAffineTerm;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.TermCompiler;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.dpll.Literal;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.proof.SourceAnnotation;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.cclosure.ArrayTheory;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.cclosure.CCAppTerm;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.theory.cclosure.CCTerm;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.util.Polynomial;
 
 /**
  * Represents a clause in the QuantifierTheory. This means, it contains at least one literal with an (implicitly)
@@ -300,9 +300,12 @@ public class QuantClause {
 						}
 					}
 				} else {
-					for (final Term smd : ((QuantBoundConstraint) atom).getAffineTerm().getSummands().keySet()) {
-						if (smd instanceof ApplicationTerm && smd.getFreeVars().length != 0) {
-							addVarArgInfo((ApplicationTerm) smd);
+					for (final Map<Term, Integer> monom : ((QuantBoundConstraint) atom).getAffineTerm().getSummands()
+							.keySet()) {
+						for (final Term factor : monom.keySet()) {
+							if (factor instanceof ApplicationTerm && factor.getFreeVars().length != 0) {
+								addVarArgInfo((ApplicationTerm) factor);
+							}
 						}
 					}
 				}
@@ -320,7 +323,6 @@ public class QuantClause {
 					lowerAffine.add(Rational.MONE);
 					final SMTAffineTerm upperAffine = new SMTAffineTerm(rhs);
 					upperAffine.add(Rational.ONE);
-					final TermCompiler compiler = mQuantTheory.getClausifier().getTermCompiler();
 					final Term lowerBound = lowerAffine.toTerm(rhs.getSort());
 					final Term upperBound = upperAffine.toTerm(rhs.getSort());
 					varInfo.addLowerGroundBound(lowerBound);
@@ -376,10 +378,12 @@ public class QuantClause {
 				}
 			}
 		} else if (func.getName() == "+" || func.getName() == "-" || func.getName() == "*") {
-			final SMTAffineTerm affine = new SMTAffineTerm(qTerm);
-			for (final Term smd : affine.getSummands().keySet()) {
-				if (smd instanceof ApplicationTerm) {
-					addVarArgInfo((ApplicationTerm) smd);
+			final Polynomial affine = new Polynomial(qTerm);
+			for (final Map<Term,Integer> smd : affine.getSummands().keySet()) {
+				for (final Term factor : smd.keySet()) {
+					if (factor instanceof ApplicationTerm) {
+						addVarArgInfo((ApplicationTerm) factor);
+					}
 				}
 			}
 		}

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/quant/QuantUtil.java
@@ -20,6 +20,7 @@ package de.uni_freiburg.informatik.ultimate.smtinterpol.theory.quant;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -28,8 +29,8 @@ import de.uni_freiburg.informatik.ultimate.logic.FunctionSymbol;
 import de.uni_freiburg.informatik.ultimate.logic.Rational;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.SMTAffineTerm;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.convert.TermCompiler;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.util.Polynomial;
 
 /**
  * This class contains helper methods to classify quantified terms and literals.
@@ -77,10 +78,12 @@ public class QuantUtil {
 				}
 				return true;
 			} else if (func.getName() == "+" || func.getName() == "-" || func.getName() == "*") {
-				final SMTAffineTerm affineTerm = SMTAffineTerm.create(term);
-				for (final Term summand : affineTerm.getSummands().keySet()) {
-					if (!isEssentiallyUninterpreted(summand)) {
-						return false;
+				final Polynomial affineTerm = new Polynomial(term);
+				for (final Map<Term, Integer> summand : affineTerm.getSummands().keySet()) {
+					for (final Term factor : summand.keySet()) {
+						if (!isEssentiallyUninterpreted(factor)) {
+							return false;
+						}
 					}
 				}
 				return true;
@@ -107,9 +110,9 @@ public class QuantUtil {
 			return containsArithmeticOnQuantOnlyAtTopLevel(((QuantBoundConstraint) atom).getAffineTerm());
 		} else {
 			final QuantEquality eq = (QuantEquality) atom;
-			final SMTAffineTerm lhsAff = new SMTAffineTerm(eq.getLhs());
+			final Polynomial lhsAff = new Polynomial(eq.getLhs());
 			if (containsArithmeticOnQuantOnlyAtTopLevel(lhsAff)) {
-				final SMTAffineTerm rhsAff = new SMTAffineTerm(eq.getRhs());
+				final Polynomial rhsAff = new Polynomial(eq.getRhs());
 				return containsArithmeticOnQuantOnlyAtTopLevel(rhsAff);
 			}
 		}
@@ -132,13 +135,19 @@ public class QuantUtil {
 			final QuantEquality eq = (QuantEquality) atom;
 			final Term lhs = eq.getLhs();
 			final Term rhs = eq.getRhs();
-			final SMTAffineTerm lhsAff = new SMTAffineTerm(lhs);
-			final SMTAffineTerm rhsAff = new SMTAffineTerm(rhs);
-			allSummandsWithoutCoeffs.addAll(lhsAff.getSummands().keySet());
-			allSummandsWithoutCoeffs.addAll(rhsAff.getSummands().keySet());
+			final Polynomial lhsAff = new Polynomial(lhs);
+			final Polynomial rhsAff = new Polynomial(rhs);
+			for (final Map<Term, Integer> monom : lhsAff.getSummands().keySet()) {
+				allSummandsWithoutCoeffs.addAll(monom.keySet());
+			}
+			for (final Map<Term, Integer> monom : rhsAff.getSummands().keySet()) {
+				allSummandsWithoutCoeffs.addAll(monom.keySet());
+			}
 		} else {
 			final QuantBoundConstraint bc = (QuantBoundConstraint) atom;
-			allSummandsWithoutCoeffs.addAll(bc.getAffineTerm().getSummands().keySet());
+			for (final Map<Term, Integer> monom : bc.getAffineTerm().getSummands().keySet()) {
+				allSummandsWithoutCoeffs.addAll(monom.keySet());
+			}
 		}
 		final Set<Term> varTerms = new HashSet<>();
 		final Set<TermVariable> varsInApps = new HashSet<>();
@@ -189,16 +198,20 @@ public class QuantUtil {
 			final QuantBoundConstraint bc = (QuantBoundConstraint) arithLit.getAtom();
 			TermVariable lowerVar = null;
 			TermVariable upperVar = null;
-			final SMTAffineTerm remainder = new SMTAffineTerm();
-			final SMTAffineTerm aff = bc.getAffineTerm();
-			for (final Entry<Term, Rational> smd : aff.getSummands().entrySet()) {
-				if (smd.getKey() instanceof TermVariable) {
+			final Polynomial remainder = new Polynomial();
+			final Polynomial aff = bc.getAffineTerm();
+			for (final Entry<Map<Term, Integer>, Rational> smd : aff.getSummands().entrySet()) {
+				Term singleton = null;
+				if (smd.getKey().size() == 1 && smd.getKey().values().iterator().next() == 1) {
+					singleton = smd.getKey().keySet().iterator().next();
+				}
+				if (singleton instanceof TermVariable) {
 					if (smd.getValue().signum() < 0) {
 						assert t1 == null;
-						lowerVar = (TermVariable) smd.getKey();
+						lowerVar = (TermVariable) singleton;
 					} else {
 						assert upperVar == null;
-						upperVar = (TermVariable) smd.getKey();
+						upperVar = (TermVariable) singleton;
 					}
 				} else {
 					remainder.add(smd.getValue(), smd.getKey());
@@ -214,12 +227,18 @@ public class QuantUtil {
 				t1 = lowerVar;
 				t2 = remainder.toTerm(lowerVar.getSort());
 			} else {
-				remainder.negate();
+				remainder.mul(Rational.MONE);
 				t1 = remainder.toTerm(upperVar.getSort());
 				t2 = upperVar;
 			}
 		}
 		return new Term[] { t1, t2 };
+	}
+
+	public static boolean isTermVariable(Map<Term, Integer> monom) {
+		return monom.size() == 1 &&
+				monom.values().iterator().next() == 1 &&
+				monom.keySet().iterator().next() instanceof TermVariable;
 	}
 
 	/**
@@ -229,10 +248,14 @@ public class QuantUtil {
 	 * @param at
 	 *            the SMTAffineTerm to check
 	 */
-	public static boolean containsArithmeticOnQuantOnlyAtTopLevel(final SMTAffineTerm at) {
-		for (final Term smd : at.getSummands().keySet()) {
-			if (!(smd instanceof TermVariable) && !isSimpleEU(smd)) {
-				return false;
+	public static boolean containsArithmeticOnQuantOnlyAtTopLevel(final Polynomial at) {
+		for (final Map<Term,Integer> monom : at.getSummands().keySet()) {
+			if (!isTermVariable(monom)) {
+				for (final Term factor : monom.keySet()) {
+					if (!isSimpleEU(factor)) {
+						return false;
+					}
+				}
 			}
 		}
 		return true;

commit 28a4384023e2de8cde4136e70bc644951ae5c4ed

Fix evaluateLAEquality negating wrong object in d565c550
In evaluateLAEquality, computing the lower bound required negating the
MutableAffineTerm `mat` before calling getUpperBound. The refactoring
in d565c550 accidentally negated `smtAff` (the Polynomial) instead,
leaving `mat` unchanged. As a result, getUpperBound was called twice
with the same un-negated `mat`, so negLowerBound == upperBound.

This caused two problems:
- FALSE detection missed the case where diff > 0 (only diff < 0 was caught)
- TRUE detection could spuriously return TRUE when diff ≤ 0 without
  verifying diff ≥ 0

Fix: use mat.negate() to match the pattern in evaluateLAEqualityKnownShared.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

SMTInterpol/src/de/uni_freiburg/informatik/ultimate/smtinterpol/theory/quant/InstantiationManager.java
@@ -1461,7 +1461,7 @@ public class InstantiationManager {
 			return mDefaultValueForLitDawgs;
 		}
 		final InfinitesimalNumber upperBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
-		smtAff.mul(Rational.MONE);
+		mat.negate();
 		final InfinitesimalNumber negLowerBound = mQuantTheory.mLinArSolve.getUpperBound(mat);
 		if (upperBound.signum() == 0 && negLowerBound.signum() == 0) {
 			return InstanceValue.TRUE;